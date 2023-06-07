93781a04c646a5060fbd2ade82cc54b4da5453e6
aryniceia <aryniceia@gmail.com>
2023-06-07 19:09:37 -0300

    Atualização do design da página

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@
 import './App.css';
 import { Routes, Route, Link } from "react-router-dom";
 import banho from './img/banho-maria.png';
+import logo from './img/logo.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="indice" element={<Indice />} />
@@ -17,74 +17,80 @@ function App() {
           <Route path="moinho" element={<MoinhoOsc />} />
           <Route path="teste" element={<teste />} />
         </Routes>
-      </header>
     </div>
   );
 }
 function Home() {
   return (
     <>
-      <main>
+    <header className="App-header">
       <div className='board-box'>
         <h2 className='title'>Olá</h2>
         <div className='text'>
           Para acessar os manuais dos maquinários, clique no botão abaixo. 
           Os manuais não são oficiais e podem conter erros. Em caso de dúvidas, contate um responsável.</div>
       </div>
-      </main>
       <nav className='box'>
-        <Link className="App-link" to="/indice">Clique Aqui!</Link>
+        <Link className="App-link" to="/indice">Clique aqui!</Link>
       </nav>
+      <img className="logo" src={logo}></img>
+    </header>
     </>
   );
 }
 function Indice() {
   return (
     <>
-      <nav className='box1'>
-        <Link className="App-link" to="/agitador">Agitador Mecânico</Link>
+    <header className="App-header">
+      <h2 className="Header-Title">Índice</h2>
+        <nav className='box1'>
+          <Link className="App-link" to="/agitador"> • Agitador Mecânico</Link>
+        </nav>
+        <nav className='box2'>
+          <Link className="App-link" to="/argamassadeira"> • Argamassadeira</Link>
+        </nav>
+        <nav className='box3'>
+          <Link className="App-link" to="/balanca"> • Balança</Link>
+        </nav>
+        <nav className='box4'>
+          <Link className="App-link" to="/banho"> • Banho Maria</Link>
+        </nav>
+        <nav className='box5'>
+          <Link className="App-link" to="/moinho"> • Moinho Oscilatório</Link>
+        </nav>
+        <nav className='box'>
+          <Link className="App-link" to="/">Voltar</Link>
       </nav>
-      <nav className='box2'>
-        <Link className="App-link" to="/argamassadeira">Argamassadeira</Link>
-      </nav>
-      <nav className='box3'>
-        <Link className="App-link" to="/balanca">Balança</Link>
-      </nav>
-      <nav className='box4'>
-        <Link className="App-link" to="/banho">Banho Maria</Link>
-      </nav>
-      <nav className='box5'>
-        <Link className="App-link" to="/moinho">Moinho Oscilatório</Link>
-      </nav>
-      <nav className='box'>
-        <Link className="App-link" to="/">Voltar</Link>
-      </nav>
+      </header>
     </>
   );
 }
 function AgitadorMec() {
   return (
     <>
-      <h2 className="Header">Agitador Mecânico</h2>
-      <p className='text1'>
-        Utilizado para misturar, dissolver ou homogeneizar soluções e suspensões de maneira eficiente e controlada<br /> Insira sua mensagem aqui... {/* O <br /> funciona como quebra de linha! */}
-        Insira sua mensagem aqui... <br /> Insira sua mensagem aqui... <br />Insira sua mensagem aqui...
-      </p>
-      <nav className='box'>
-        <Link className="App-link" to="/argamassadeira">Avançar</Link>
-      </nav>
-      <nav className='box'>
-        <Link className="App-link" to="/indice">Voltar</Link>
-      </nav>
-      <nav className='box'>
-        <Link className="App-link" to="/indice">Índice</Link>
-      </nav>
+      <header className="App-header">
+        <h2 className="Header">Agitador Mecânico</h2>
+        <p className='text1'>
+          Utilizado para misturar, dissolver ou homogeneizar soluções e suspensões de maneira eficiente e controlada<br /> Insira sua mensagem aqui... {/* O <br /> funciona como quebra de linha! */}
+          Insira sua mensagem aqui... <br /> Insira sua mensagem aqui... <br />Insira sua mensagem aqui...
+        </p>
+        <nav className='box'>
+          <Link className="App-link" to="/argamassadeira">Avançar</Link>
+        </nav>
+        <nav className='box'>
+          <Link className="App-link" to="/indice">Voltar</Link>
+        </nav>
+        <nav className='box'>
+          <Link className="App-link" to="/indice">Índice</Link>
+        </nav>
+      </header>
     </>
   );
 }
 function Argamassadeira() {
   return (
     <>
+    <header className="App-header">
       <h2 className="Header">Argamassadeira</h2>
       <p className="Header">Insira sua mensagem aqui...</p>
       <nav className='box'>
@@ -96,12 +102,14 @@ function Argamassadeira() {
       <nav className='box'>
         <Link className="App-link" to="/indice">Índice</Link>
       </nav>
+    </header>
     </>
   );
 }
 function Balança() {
   return (
     <>
+    <header className="App-header">
         <h2 className="Header">Balança</h2>
         <p className='text1'>  Insira sua mensagem aqui...<br /> Insira sua mensagem aqui... {/* O <br /> funciona como quebra de linha! */}
           Insira sua mensagem aqui...  <br /> Insira sua mensagem aqui...
@@ -118,6 +126,7 @@ function Balança() {
       <nav className='box'>
         <Link className="App-link" to="/indice">Índice</Link>
       </nav>
+    </header>
     </>
   );
 }
@@ -165,6 +174,7 @@ function BanhoMaria() {
 function MoinhoOsc() {
   return (
     <>
+    <header className="App-header">
       <h2 className="Header">Moinho Oscilatório</h2>
       <p className="Header">Insira sua mensagem aqui...</p>
       <nav className='box'>
@@ -176,6 +186,7 @@ function MoinhoOsc() {
       <nav className='box'>
         <Link className="App-link" to="/indice">Índice</Link>
       </nav>
+    </header>
     </>
   );
 }
