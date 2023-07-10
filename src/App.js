788987478eeb8a57672dd7e21cb47bd018f874fe
aryniceia <aryniceia@gmail.com>
2023-07-10 20:01:07 -0300

    adição de responsividade

--- src/App.js
+++ src/App.js
@@ -76,33 +76,33 @@ function AgitadorMec() {
     <>
       <header className="App-header1">
         <h2 className="Header-Title">Agitador Mecânico</h2>
-  
-          <li className="Header">
-            ⚠️ Atenção, antes de usar verifique: ⚠️ <br></br>
-              ● A voltagem do equipamento; <br></br>
-              ● Se a tomada está devidamente ligada e aterrada. <br></br><br></br>
+        <div className='row'>
+          <div className='col-1'></div>
+          <ul className="Header">
+            <li>⚠️ Atenção, antes de usar verifique: ⚠️</li>
+            <li> A voltagem do equipamento;</li>
+            <li> Se a tomada está devidamente ligada e aterrada.</li> <br></br>
 
-              ● Para ligar o equipamento, pressione a tecla de on/off por 3 segundos; <br></br>
-              ● Assim, será apresentado no display simultaneamente as seguintes informações: <br></br>
-                  Rotação (RPM), Tempo (Min) e Modo. <br></br> 
-              ● A tecla Set (4), navega-se entre os parâmetros de programação; <br></br>
-              ● Primeiramente seleciona-se o modo, ficará piscando no display MODE 1, <br></br> 
-                com as setas selecione o modo desejado (de 1 a 3); <br></br>
-              ● Pressione mais uma vez a tecla Set e ficará piscando <br></br> 
-                a rotação (RPM) e com as setas selPressione Set mais <br></br> 
-                uma vez e ficará piscando o tempo (Min), com as setas <br></br> 
-                programe o tempo desejado. Se deixar o tempo zerado, <br></br> 
-                o equipamento agitará continuamente, sem contagem de <br></br> 
-                tempo para desligar automaticamente.Para modificar a <br></br> 
-                velocidade de subida (RPM / min) gire o controlador de velocidade (3); <br></br>
-              ● As programações ficarão salvas para cada <br></br> 
-                modo: MODE 1, MODE 2 e MODE 3. <br></br>
-                Para iniciar a agitação e/ou contagem de <br></br> 
-                tempo, pressione a tecla onf/off . Caso queira pausar <br></br>
-                a rotação, basta pressionar a tecla on/off em qualquer momento. <br></br>
-                Para desligar, pressione on/off por 3 segundos a tecla . <br></br>
-              
-          </li>
+            <li>● Para ligar o equipamento, pressione a tecla de on/off por 3 segundos; </li>
+            <li> ● Assim, será apresentado no display simultaneamente as seguintes informações:
+                  Rotação (RPM), Tempo (Min) e Modo. </li>
+            <li>● A tecla Set (4), navega-se entre os parâmetros de programação; </li> 
+            <li>● Primeiramente seleciona-se o modo, ficará piscando no display MODE 1, 
+                com as setas selecione o modo desejado (de 1 a 3);  
+                a rotação (RPM) e com as setas selPressione Set mais 
+                uma vez e ficará piscando o tempo (Min), com as setas  
+                programe o tempo desejado. Se deixar o tempo zerado,  
+                o equipamento agitará continuamente, sem contagem de 
+                tempo para desligar automaticamente.Para modificar a  
+                velocidade de subida (RPM / min) gire o controlador de velocidade (3); </li> 
+              <li>● As programações ficarão salvas para cada 
+                modo: MODE 1, MODE 2 e MODE 3. 
+                Para iniciar a agitação e/ou contagem de  
+                tempo, pressione a tecla onf/off . Caso queira pausar 
+                a rotação, basta pressionar a tecla on/off em qualquer momento. 
+                Para desligar, pressione on/off por 3 segundos a tecla.</li> 
+          </ul>
+          </div>
         <img className='agitador'src={agitador}></img>
 
         <nav className='box'>
@@ -123,22 +123,34 @@ function Argamassadeira() {
     <>
     <header className="App-header2">
       <h2 className="Header-Title">Argamassadeira</h2>
-      <p className="Header">⚠️ Atenção, antes de usar verifique: ⚠️ <br></br><br></br>
-        ● Posicionar o equipamento em bancada firme e nivelada; <br></br>
-        ●  Certificar-se de que a pá está bem fixada ao eixo; <br></br>
-        Certificar-se da tensão de alimentação elétrica e de <br></br>
-        que a chave elétrica localizada no lado esquerdo da <br></br> 
-        coluna está desligada (pos. 0 ) e conectar o equipamento à rede. <br></br><br></br>
+      <div className='row'>
+      <div className='col-1'/>
+        <ul className="Header">
+          <li>⚠️ Atenção, para a operação existem dois modos: ⚠️</li>
+            <li>Manual</li>
+            <li>Automático</li> <br></br>
 
-        ● Posicionar o localizador da cuba no encaixe da base móvel; <br></br> 
-        Travar a cuba na base móvel girando para a direita a alavanca plástica, <br></br>
-        sem esforço excessivo;<br></br>
-        ● Trazer a alavanca de elevação para a frente até a sua posição máxima; <br></br>
-        ● Acionar o equipamento por meio da chave elétrica de 3 posições, <br></br> 
-        girando o botão para a posição1 (baixa rotação) <br></br> 
-        ou 2 (alta rotação), conforme especificação a ser seguida; <br></br>
-        ● Retornar o botão da chave elétrica para a posição zero para finalizar a operação.
-      </p>
+          <li>Automático:</li>
+            <li>●  Pressione o botão "INICIAR AUTO" no painel de controle digital;</li>
+            <li>● O equipamento irá iniciar a execução do ensaio automaticamente, segundo 
+              procedimentos da NBR 7215:1996 da seguinte forma:
+              Iniciará a homogeneização prévia da mistura de cimento e água por 30 segundos 
+              na velocidade "lento" formando a pasta de cimento, logo após acenderá a luz de 
+              sinalização de adição da areia (luz amarela), indicando o momento de adição da 
+              areia, depois homogeneizará a pasta de cimento com areia por 30 segundos na 
+              velocidade "lento", em seguida passará para a velocidade “rápida” por mais 30 
+              segundos e ao término deste tempo irá ficar na posição de repouso (parado) por 
+              90 segundos para que seja executados procedimentos na NBR 7215:1996. “Após 
+              o tempo de repouso o equipamento irá reiniciar a homogeneização da mistura de 
+              argamassa na velocidade rápida” por mais 60 segundos, em seguida encerrará o 
+              processo. Todo o procedimento levará um total de 240 segundos.</li>
+        <li>Manual:</li>
+          <li>● Pressione a tecla “AUMENTAR” uma vez para acionar a velocidade lenta;</li>
+          <li>● Pressione mais uma vez para acionar a velocidade rápido;</li>
+          <li>● Pressione a tecla “PARAR” ou “RESETAR”, para finalizar o processo;</li>
+          <li>● Dessa forma será posível controlar o tempo de execução e a velocidade do ensaio de acordo com os seus requisitos.</li>
+        </ul>
+      </div>
       <img className='argamassadeira'src={argamassadeira}></img>
       <nav className='box'>
         <Link className="App-link" to="/balanca">Avançar</Link>
@@ -178,26 +190,29 @@ function BanhoMaria() {
     <>
     <header className='App-header4'>
       <h2 className="Header-Title">Banho Maria</h2>
-      <p className="Header">
-        ⚠️ Atenção, antes de usar verifique: ⚠️ <br></br><br></br>
-            ● A voltagem do equipamento; <br></br>
-            ● Se o equipamento possui o fluido interno. <br></br><br></br>
+      <div className='row'>
+        <div className='col-1'></div>
+        <ul className="Header">
+          ⚠️ Atenção, antes de usar verifique: ⚠️ <br></br><br></br>
+              ● A voltagem do equipamento; <br></br>
+              ● Se o equipamento possui o fluido interno. <br></br><br></br>
 
-            Após isso, siga as instruções de operação:<br></br>
+              Após isso, siga as instruções de operação:<br></br>
 
-              ● Conecte o cabo de alimentação na rede elétrica; <br></br>
-              ● Insira o fluido de trabalho na cuba sem encher demais. Mantenha no mínimo <br></br>
-              1 cm de distância entre o fluido e a borda da cuba; <br></br>
-              ● Ligue a chave geral na posição "ligado"; <br></br>
-              ● Acione a chave de agitação. Lembre-se de inserir a <br></br> 
-              pastilha de teflon no interior da cuba;<br></br>
-              ● Configure o controlador de temperatura usandoas <br></br>
-              teclas de incremento e decremento; <br></br>
-              ● Note que ao teclar o visor de temperatura ofertada <br></br> 
-              irá oscilar, configure na temperatura de trabalho. <br></br>
-              Em seguida, o visor de temperatura interna irá começar <br></br>
-              a variar de acordo com a configuração.
-      </p>
+                ● Conecte o cabo de alimentação na rede elétrica; <br></br>
+                ● Insira o fluido de trabalho na cuba sem encher demais. Mantenha no mínimo <br></br>
+                1 cm de distância entre o fluido e a borda da cuba; <br></br>
+                ● Ligue a chave geral na posição "ligado"; <br></br>
+                ● Acione a chave de agitação. Lembre-se de inserir a <br></br> 
+                pastilha de teflon no interior da cuba;<br></br>
+                ● Configure o controlador de temperatura usandoas <br></br>
+                teclas de incremento e decremento; <br></br>
+                ● Note que ao teclar o visor de temperatura ofertada <br></br> 
+                irá oscilar, configure na temperatura de trabalho. <br></br>
+                Em seguida, o visor de temperatura interna irá começar <br></br>
+                a variar de acordo com a configuração.
+        </ul>
+      </div>
       <img className='banho-maria'src={banho}></img>
       <nav className='box'>
         <Link className="App-link" to="/moinho">Avançar</Link>
